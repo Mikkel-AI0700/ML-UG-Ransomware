732c016c8b02454ed0a734eb8ca97d7aadc25574
Mikkel-AI0700 <mikeruai0700@gmail.com>
2026-03-08 11:33:39 +0800

    Fixed multiple logical issues that stopped training

--- training-scripts/training/train_ffm_opt.py
+++ training-scripts/training/train_ffm_opt.py
@@ -1,20 +1,22 @@
 import pandas as pd
 import torch
 import torch.nn
-from torch.utils.data import Dataset
+import torch.optim as optim
+from torch.nn.modules.linear import Linear
+from torch.utils.data import Dataset, DataLoader
 import torch.nn.functional as torch_func
 
 class LoadData (Dataset):
     def __init__ (self, training_x: str, training_y: str):
-        self.train_x = pd.read_csv(training_x)
-        self.train_y = pd.read_csv(training_y)
+        self.train_x = pd.read_csv(training_x).to_numpy()
+        self.train_y = pd.read_csv(training_y).to_numpy()
 
-    def __len_ (self):
+    def __len__ (self):
         return len(self.train_x)
 
     def __getitem__ (self, index):
-        train_x_sample = self.train_x.iloc[index]
-        train_y_sample = self.train_y.iloc[index]
+        train_x_sample = self.train_x[index]
+        train_y_sample = self.train_y[index]
         
         return (
             torch.tensor(train_x_sample, dtype=torch.float32),
@@ -25,38 +27,63 @@ class FeedForwardRansomware (torch.nn.Module):
     def __init__ (self):
         super(FeedForwardRansomware, self).__init__()
         self.module_dict_linear = torch.nn.ModuleDict({
-            "linear_layer_input": torch.nn.Linear(in_features=14, out_features=32, device="cuda"),
-            "linear_layer_1": torch.nn.Linear(in_features=32, out_features=64, device="cuda"),
-            "linear_layer_2": torch.nn.Linear(in_features=64, out_features=64, device="cuda"),
-            "linear_layer_3": torch.nn.Linear(in_features=64, out_features=64, device="cuda"),
-            "linear_layer_4": torch.nn.Linear(in_features=64, out_features=3, device="cuda")
+            "linear_layer_input": Linear(in_features=8, out_features=32, device="cuda"),
+            "linear_layer_1": Linear(in_features=32, out_features=64, device="cuda"),
+            "linear_layer_2": Linear(in_features=64, out_features=128, device="cuda"),
+            "linear_layer_3": Linear(in_features=128, out_features=128, device="cuda"),
+            "linear_layer_4": Linear(in_features=128, out_features=32, device="cuda"),
+            "linear_layer_5": Linear(in_features=32, out_features=32, device="cuda"),
+            "linear_layer_output": Linear(in_features=32, out_features=3, device="cuda")
         })
 
     def forward (self, dataset_samples):
-        tensor_output = None
+        tensor_output = dataset_samples
         for layer_name, layer_ref in self.module_dict_linear.items():
-            tensor_output = layer_ref(dataset_samples)
-            tensor_output = torch.nn.functional.relu(tensor_output)
+            tensor_output = layer_ref(tensor_output)
+            tensor_output = torch_func.relu(tensor_output)
             
-            if "layer" in layer_name:
-                tensor_output = torch.nn.functional.dropout(tensor_output, p=0.15)
+            if "input" in layer_name or "output" in layer_name:
+                tensor_output = torch_func.dropout(tensor_output, p=0.15)
             else:
-                tensor_output = torch.nn.functional.dropout(tensor_output, p=0.50)
+                tensor_output = torch_func.dropout(tensor_output, p=0.50)
                 
         return tensor_output
     
-def _train_feedforward (
-    optuna_trial,
-    train_x: str,
-    train_y: str,
-):
-    pass
-
 def main ():
-    tr_x = pd.read_csv("../../datasets/tr_x.csv")
-    tr_y = pd.read_csv("../../datasets/tr_y.csv")
-    ts_x = pd.read_csv("../../datasets/ts_x.csv")
-    ts_y = pd.read_csv("../../datasets/ts_y.csv")
-    
-    
+    train_set = LoadData("../../datasets/tr_x.csv", "../../datasets/tr_y.csv")
+    train_load_generator = DataLoader(
+        train_set, 
+        batch_size=2048, 
+        shuffle=True, 
+        num_workers=120, 
+        pin_memory=True,
+        prefetch_factor=50,
+        persistent_workers=True
+    )
 
+    ffr_model = FeedForwardRansomware()
+
+    cross_entropy_loss = torch.nn.CrossEntropyLoss()
+    adam_optim = optim.Adam(ffr_model.parameters(), lr=0.0001)
+
+    ffr_model.to("cuda")
+
+    for epoch in range(3500):
+        accumulated_criterion_loss = 0.0
+        for train_feature, train_label in train_load_generator:
+            train_feature = train_feature.to("cuda")
+            train_label = train_label.to("cuda")
+
+            adam_optim.zero_grad()
+            model_prediction = ffr_model(train_feature)
+            loss = cross_entropy_loss(model_prediction, train_label.flatten().long())
+            loss.backward()
+            adam_optim.step()
+
+            accumulated_criterion_loss += loss.item()
+
+        averaged_accumulated_loss = accumulated_criterion_loss / len(train_load_generator)
+
+        print(f"Epoch: {epoch} | Loss: {(averaged_accumulated_loss * 100)}")
+
+main()
